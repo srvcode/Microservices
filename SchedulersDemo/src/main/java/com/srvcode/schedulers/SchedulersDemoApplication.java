f7cb3dca61c0bf1cec36aa28438b9b7306fb3048
Shivam-Omen <srvcode@gmail.com>
2023-03-18 14:17:54 +0530

    NA

--- SchedulersDemo/src/main/java/com/srvcode/schedulers/SchedulersDemoApplication.java
+++ SchedulersDemo/src/main/java/com/srvcode/schedulers/SchedulersDemoApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+/*
+ * @EnableScheduling annotation tells Spring that this particular application
+ * will run tasks at scheduled intervals;
+ * 
+ * This annotation will bring up a task executor to run the scheduled task
+ * on a background thread, not on the main thread.
+ */
 @EnableScheduling
 public class SchedulersDemoApplication {
 
